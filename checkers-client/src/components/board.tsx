484c1201cd1a3b89cfd081917b6785dd821b8dfe
Jason R. Carrete <jasoncarrete5@gmail.com>
2020-10-19 12:49:44 -0400

    Switch to using pure TypeScript

--- checkers-client/src/components/board.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Board = () => {
-  return <div>This is the board of the game</div>;
-};
-
-export default Board;
---
+++ checkers-client/src/index.ts
@@ -0,0 +1,9 @@
+const createGame = document.getElementById('btn-create-game') as HTMLButtonElement
+createGame.addEventListener('click', ev => {
+
+})
+
+const joinGame = document.getElementById('btn-join-game') as HTMLButtonElement
+joinGame.addEventListener('click', ev => {
+    const gameCode = prompt("Enter a game code")
+})
