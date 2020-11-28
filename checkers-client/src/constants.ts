d871684e9c777ab3a3246aeeec6193371a908df5
Loc Lam <shenlong3010@gmail.com>
2020-11-28 14:25:52 -0500

    seperated enums, constants and boardCanvas to different files

---
+++ checkers-client/src/constants.ts
@@ -0,0 +1,16 @@
+/*************
+ * CONSTANTS
+ *************/
+
+import { boardCanvas } from './boardCanvas';
+
+export const LOCAL_MAN_COLOR = '#edf285';
+export const LOCAL_KING_COLOR = '#000000';
+export const REMOTE_MAN_COLOR = '#fd8c04';
+export const REMOTE_KING_COLOR = '#f4f4f2';
+export const DARK_SPACE_COLOR = '#8db596';
+export const LIGHT_SPACE_COLOR = '#bedbbb';
+export const PIECE_SELECTION_BORDER_COLOR = 'red';
+export const FREE_SPACE_SELECTION_BORDER_COLOR = 'blue';
+export const SELECTION_BORDER_WIDTH = 2;
+export const SIDE_LEN = boardCanvas.width / 8;
