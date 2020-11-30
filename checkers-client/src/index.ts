64510902699e952c4a15953509b688d1443a1c16
Jason R. Carrete <jasoncarrete5@gmail.com>
2020-11-30 00:08:26 -0500

    Remove create game and join game

--- checkers-client/src/index.ts
+++ checkers-client/src/index.ts
@@ -5,20 +5,5 @@
  */
 
 import { drawBoard } from './gameboard'
-import { hostGame, joinGame } from './p2p'
-
-const createGameBtn = document.getElementById('btn-create-game') as HTMLButtonElement
-const joinGameBtn = document.getElementById('btn-join-game') as HTMLButtonElement
-
-/* btn-create-game click event listener */
-createGameBtn.addEventListener('click', async ev => {
-    hostGame()
-})
-
-/* btn-join-game click event listener */
-joinGameBtn.addEventListener('click', async ev => {
-    const gameCode = prompt('Enter a game code')
-    joinGame(gameCode)
-})
 
 drawBoard()
